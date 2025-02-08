dad7d2192042bcc3b3e2a6102ab6c63debddc8af
oligregz <gregorysak90@gmail.com>
2025-02-08 17:56:47 -0300

    fix: change entity imports

--- api/src/db/module/db.module.ts
+++ api/src/db/module/db.module.ts
@@ -1,20 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { EventEntity } from '../entities/event.entity';
+import { UserEntity } from '../entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRootAsync({
-    useFactory: async (configService: ConfigService, param) => ({
-      type: 'postgres',
-      host: configService.get<string>('DB_HOST'),
-      port: +configService.getOrThrow<number>('DB_PORT'),
-      username: configService.get<string>('DB_USERNAME'),
-      password: configService.get<string>('DB_PASSWORD'),
-      database: configService.get<string>('DB_NAME'),
-      entities: [__dirname + '/enities/**'],
-      migrations: [__dirname + '/migrations/*.ts'],
-      synchronize: false
-    }),
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService, param) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: +configService.getOrThrow<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [EventEntity, UserEntity],
+        migrations: [__dirname + '/migrations/*.ts'],
+        synchronize: false
+      }
+    ),
     inject: [ConfigService]
   })]
 })
