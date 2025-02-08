d4bbab6bd9e3b64895a3cecd8d99e14dde2fe3ab
oligregz <gregorysak90@gmail.com>
2025-02-08 20:18:31 -0300

    file/fix: change name file and entites import

--- api/src/db/typeOrm.migration-config.ts
+++ api/src/db/typeOrm.migration-config.ts
@@ -1,11 +1,12 @@
 import { ConfigService } from "@nestjs/config";
 import { config } from "dotenv";
 import { DataSource, DataSourceOptions } from "typeorm";
+import { EventEntity } from "./entities/event.entity";
+import { UserEntity } from "./entities/user.entity";
 
 config();
 
 const configService = new ConfigService();
-
 const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   host: configService.get<string>('DB_HOST'),
@@ -13,9 +14,8 @@ const dataSourceOptions: DataSourceOptions = {
   username: configService.get<string>('DB_USERNAME'),
   password: configService.get<string>('DB_PASSWORD'),
   database: configService.get<string>('DB_NAME'),
-  entities: [],
+  entities: [EventEntity, UserEntity],
   migrations: [__dirname + '/migrations/*.ts'],
-  synchronize: false
-}
+};
 
 export default new DataSource(dataSourceOptions);
