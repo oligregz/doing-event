52a8558d71ae9387a55543dbdadc76fd4862ec96
oligregz <gregorysak90@gmail.com>
2025-02-11 16:25:55 -0300

    feat/style: add conditional rendering in button and change dialog background with button color

--- interface/src/components/dialog/Dialog.tsx
+++ interface/src/components/dialog/Dialog.tsx
@@ -5,18 +5,21 @@ interface DialogProps {
   isOpen: boolean;
   onClose: () => void;
   children: React.ReactNode;
+  showCloseButton: boolean;
 }
 
-function Dialog({ isOpen, onClose, children }: DialogProps) {
+function Dialog({ isOpen, onClose, children, showCloseButton }: DialogProps) {
   if (!isOpen) return null;
 
   return (
     <div className="dialog-overlay">
       <div className="dialog">
         <div className="dialog-content">{children}</div>
-        <button className="dialog-close-btn" onClick={onClose}>
-          Close
-        </button>
+        {showCloseButton && (
+          <button className="dialog-close-btn" onClick={onClose}>
+            Close
+          </button>
+        )}
       </div>
     </div>
   );
--- interface/src/components/signin/Signin.tsx
+++ interface/src/components/signin/Signin.tsx
@@ -1,38 +1,54 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "../../styles/styles.css";
 import DiamondPNG from "../../../assets/diamond.png";
 import { AxiosResponse } from "axios";
 import signinService from "./signin.service";
+import Dialog from "../dialog/Dialog";
 
 export function Signin() {
-  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [signinResponse, setSigninResponse] = useState<AxiosResponse | null>(
     null
   );
-  const [signinData, setSigninData] = useState({});
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [showCloseButton, setShowCloseButton] = useState(false);
 
-  useEffect(() => {
-    console.log(signinData);
-  }, [signinData]);
+  const handleSignin = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-  const handleSignin = async () => {
+    setIsLoading(true);
+    setShowCloseButton(false);
+
     try {
       const data = {
-        email,
+        username: name,
         password,
       };
-      setSigninData(data);
       const signinRes = await signinService(data);
 
-      if (signinRes) {
-        setSigninResponse(signinRes);
-        console.log(signinResponse);
+      if (!signinRes.token) {
+        setErrorMessage(
+          signinRes || "Login failed. Please check your credentials."
+        );
+        setIsDialogOpen(true);
       }
+      setSigninResponse(signinRes);
       console.log(signinResponse);
     } catch (error) {
       console.error(error);
+      setErrorMessage("An error occurred. Please try again later.");
+      setIsDialogOpen(true);
+    } finally {
+      setIsLoading(false);
+      setShowCloseButton(true);
     }
+  };
+
+  const closeDialog = () => {
+    setIsDialogOpen(false);
   };
 
   return (
@@ -42,27 +58,23 @@ export function Signin() {
           <span className="login-form-title">Welcome</span>
 
           <span className="login-form-title">
-            <img src={DiamondPNG} />
+            <img src={DiamondPNG} alt="Diamond" />
           </span>
 
           <div className="wrap-input">
             <input
-              className={email !== "" ? "has-val input" : "input"}
-              type="email"
-              name=""
-              id=""
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              className={name !== "" ? "has-val input" : "input"}
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
-            <span className="focus-input" data-placeholder="Email"></span>
+            <span className="focus-input" data-placeholder="Username"></span>
           </div>
 
           <div className="wrap-input">
             <input
-              className={email !== "" ? "has-val input" : "input"}
+              className={name !== "" ? "has-val input" : "input"}
               type="password"
-              name=""
-              id=""
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
@@ -84,6 +96,18 @@ export function Signin() {
           </div>
         </form>
       </div>
+
+      <Dialog
+        isOpen={isDialogOpen}
+        onClose={closeDialog}
+        showCloseButton={showCloseButton}
+      >
+        <p>{errorMessage}</p>
+      </Dialog>
+
+      <Dialog isOpen={isLoading} onClose={() => {}} showCloseButton={false}>
+        <p>Loading...</p>
+      </Dialog>
     </div>
   );
 }
